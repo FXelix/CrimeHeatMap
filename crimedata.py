6ad28558bb83ae8a745ad03762a0cf5ad668f7d7
David Lindley <david.lindley5@gmail.com>
2017-09-19 16:55:48 -0400

    Data normalized across time frame

--- crimedata.py
+++ crimedata.py
@@ -1,14 +1,14 @@
 import csv
 import time
 
-import gmplot
+import gmplot.gmplot.gmplot as gmplot
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 
 def create_data_frame(csv_file_name, years):
-	#creates crime_locations.csv with data for specific year to increase performance
+	# creates crime_locations.csv with data for specific year to increase performance
 	with open(csv_file_name, 'r') as file_in, open('crime_locations.csv', 'w') as  file_out:
 		reader = csv.reader(file_in)
 		writer = csv.writer(file_out)
@@ -26,8 +26,9 @@ def create_data_frame(csv_file_name, years):
 	df = df.sort_values('Date')
 	return df
 
+
 def filter_data_by_month(df, year, month):
-	#takes year-range dataframe and breaks it down into month
+	# takes year-range dataframe and breaks it down into month
 	next_month = month + 1
 	if next_month != 13:
 		# error handling for December/January
@@ -40,7 +41,7 @@ def filter_data_by_month(df, year, month):
 
 
 def get_lat_long(dataframe):
-	#coverts dataframe lat/longs into lists
+	# coverts dataframe lat/longs into lists
 	lat_long = (dataframe[dataframe.Latitude.notnull()])
 	lats = lat_long['Latitude'].tolist()
 	longs = lat_long['Longitude'].tolist()
@@ -48,17 +49,16 @@ def get_lat_long(dataframe):
 
 
 def generate_map(lats, longs, map_name):
-	#draws map and saves .html file
+	# draws map and saves .html file
 	my_map = gmplot.GoogleMapPlotter(41.8486592, -87.707648, 11)
-	my_map.heatmap(lats, longs, radius=16)
+	my_map.heatmap(lats, longs,radius=16,maxIntensity=80)
 	my_map.draw(f'html_files/{map_name}.html')
-	print('html file saved')
 	return map_name
 
 
 def open_map(map_name, browser):
-	#opens map in selenium browser and saves screenshot as year-month.png
-	url = f'html_files/{map_name}.html'
+	# opens map in selenium browser and saves screenshot as year-month.png
+	url = f'file:///Users/Dave/Desktop/PythonProjects/CrimeHeatMap/html_files/{map_name}.html'
 	browser.get(url)
 	file_loaded = '//*[@id="map_canvas"]/div/div/div[10]/div[2]/div[1]'
 	WebDriverWait(browser, 10).until(lambda driver: driver.find_element_by_xpath(file_loaded))
@@ -71,26 +71,26 @@ def run(start_year, end_year, csv_file_name):
 	df = create_data_frame(csv_file_name, years)
 	names = []
 	browser = webdriver.Chrome()
-	#iterates through months in years
+	# iterates through months in years
 	for year in range(start_year, end_year):
 		for month in range(1, 13):
-			#convert monthly datarance flat-file to dataframe
+			# convert monthly datarance flat-file to dataframe
 			monthly_data = filter_data_by_month(df, year, month)
-			#get lat/longs for that month as lists
+			# get lat/longs for that month as lists
 			lats, longs = get_lat_long(monthly_data)
-			#generates .html map file and adds map name to names list
+			# generates .html map file and adds map name to names list
 			names.append(generate_map(lats, longs, (f'{year} - {month}')))
+	print(f'html files generated:{names}')
 	for map_name in names:
-		#iterates through saved maps (names), generating a map screenshot for each month
+		# iterates through saved maps (names), generating a map screenshot for each month
 		open_map(map_name, browser)
 
 
 year_range = []
-for i in range(2000, 2017, 4):
-	my_tup = (i, i + 4)
+for i in range(2010, 2016, 1):
+	my_tup = (i, i + 1)
 	year_range.append(my_tup)
 
 for year in year_range:
 	run(year[0], year[1], 'Crime.csv')
 	print(f'{year[0]} to {year[1]} has been processed')
-
