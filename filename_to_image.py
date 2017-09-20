2779c5c437b71a464f025018c2d48c49a2e37322
David Lindley <david.lindley5@gmail.com>
2017-09-20 18:20:06 -0400

    Data normalized across time frame, headless browser implemented.

--- filename_to_image.py
+++ filename_to_image.py
@@ -4,7 +4,7 @@ def add_filename(image_name):
 	im = Image.open(f'png_images/{image_name}.png')
 	draw = ImageDraw.Draw(im)
 	font = ImageFont.truetype('abel-regular.ttf',30)
-	draw.text((256,480),f'{image_name}',(0,0,0),font=font)
+	draw.text((700,256),f'{image_name}',(0,0,0),font=font)
 	im.save(f'png_images/dated/ ({image_name}).png')
 
 
